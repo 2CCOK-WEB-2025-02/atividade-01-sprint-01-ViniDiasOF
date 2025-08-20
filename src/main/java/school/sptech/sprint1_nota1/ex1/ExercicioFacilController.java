e3f46bc7cb3dfaabc2b38a106c730121a4fadb42
Vinícius Dias <vinicius.dsilva@sptech.school>
2025-08-20 09:58:18 -0300

    Atividades concluídas

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,27 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ExercicioFacilController {
+        @RestController
+        public class ExercicioFacilController {
 
-    @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
-    }
-}
+            @GetMapping("/ex-01/{palavra}")
+            public Boolean exercicioFacil(@PathVariable String palavra) {
+                String normalizada = palavra.toLowerCase();
+
+                int i = 0;
+                int j = normalizada.length() - 1;
+
+                while (i < j) {
+                    if (normalizada.charAt(i) != normalizada.charAt(j)) {
+                        return false;
+                    }
+                    i++;
+                    j--;
+                }
+
+                return true;
+            }
+        }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -8,7 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse calcularFibonacci(@PathVariable int n) {
+        if (n <= 0) {
+            return new ExercicioDificilResponse(0, 0);
+        }
+
+        int termoAnterior = 0;
+        int termoAtual = 1;
+        int soma = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int proximo = termoAnterior + termoAtual;
+            termoAnterior = termoAtual;
+            termoAtual = proximo;
+            soma += termoAtual;
+        }
+
+        return new ExercicioDificilResponse(termoAtual, soma);
     }
 }
